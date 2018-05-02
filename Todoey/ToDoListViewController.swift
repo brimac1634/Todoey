62d1d161e77d743d99956309260e47adfc55b61f
Brian MacPherson <brimac1634@gmail.com>
2018-05-02 12:00:54 +0800

    Added "Slide to delete" capability.

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -50,6 +50,15 @@ class ToDoListViewController: UITableViewController {
         
     }
     
+    
+    //Slide to delete method
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.itemArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
     //MARK - Add new items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
