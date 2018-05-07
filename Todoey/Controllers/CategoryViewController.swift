b56583504f29ba6e98b0e525c8931a550bdab639
Brian MacPherson <brimac1634@gmail.com>
2018-05-07 16:48:32 +0800

    Added Categories VC and setup tableview

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,106 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Brian MacPherson on 7/5/2018.
+//  Copyright © 2018 Brian MacPherson. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    var categoriesArray : [Category] = []
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loadData()
+    }
+    
+    
+    //MARK: - Tableview Data Source Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoriesArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categoriesArray[indexPath.row].name
+        return cell
+    }
+    
+    //MARK: - Data Manipulation Methods
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let alert = UIAlertController(title: "Delete List", message: "Are you sure you want to delete the \"\(self.categoriesArray[indexPath.row].name!)\" list?", preferredStyle: .alert)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .default) { (action) in
+                self.context.delete(self.categoriesArray[indexPath.row])
+                self.categoriesArray.remove(at: indexPath.row)
+                self.updateData()
+            }
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            }
+            
+            alert.addAction(deleteAction)
+            alert.addAction(cancelAction)
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    //MARK: - Add New Categories
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New ToDoey Category", message: "", preferredStyle: .alert)
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Category"
+            textField = alertTextField
+        }
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            let newCategory = Category(context: self.context)
+            
+            newCategory.name = textField.text
+            self.categoriesArray.append(newCategory)
+            
+            
+            self.updateData()
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+   
+    
+    //MARK: - Tableview Delegate Methods
+    
+    
+    //MARK: - Functions
+    func updateData() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving data, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadData() {
+        let request : NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do {
+            categoriesArray = try context.fetch(request)
+        } catch {
+            print("Error loading data, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+}
--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -15,7 +15,6 @@ class ToDoListViewController: UITableViewController {
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,61 +26,61 @@ class ToDoListViewController: UITableViewController {
     
 
     
-    //MARK - Tableview Datasource Methods
+    //MARK: - Tableview Datasource Methods
     
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        
-        let item = itemArray[indexPath.row]
-        cell.textLabel?.text = item.title
-        
-        // Ternary operator>
-        // value = condition ? valueIfTrue : ValueIfFalse
-        
-        cell.accessoryType = item.isDone ? .checkmark : .none
-        
-        //Use ternary operator above to represent the coding below. much shorter
-//        if item.isDone == true {
-//            cell.accessoryType = .checkmark
-//        } else {
-//            cell.accessoryType = .none
-//        }
-        return cell
-    }
-
-
-    //MARK - Tableview Delegate Methods
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //print(itemArray[indexPath.row])
-        
-    
-        itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
-        
-        updateData()
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-    }
-    
-    
-    //Slide to delete method
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-
-            context.delete(itemArray[indexPath.row])
-            itemArray.remove(at: indexPath.row)
-            updateData()
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return itemArray.count
         }
-    }
+    
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+            
+            let item = itemArray[indexPath.row]
+            cell.textLabel?.text = item.title
+            
+            // Ternary operator>
+            // value = condition ? valueIfTrue : ValueIfFalse
+            
+            cell.accessoryType = item.isDone ? .checkmark : .none
+            
+//            Use ternary operator above to represent the coding below. much shorter
+//            if item.isDone == true {
+//                cell.accessoryType = .checkmark
+//            } else {
+//                cell.accessoryType = .none
+//            }
+            return cell
+        }
+
+
+    //MARK: - Tableview Delegate Methods
+    
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            //print(itemArray[indexPath.row])
+            
+        
+            itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
+            
+            updateData()
+            
+            tableView.deselectRow(at: indexPath, animated: true)
+            
+        }
     
     
-    //MARK - Add new items
+        //Slide to delete method
+        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+
+                context.delete(itemArray[indexPath.row])
+                itemArray.remove(at: indexPath.row)
+                updateData()
+            }
+        }
+    
+    
+    //MARK: - Add new items
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -118,6 +117,7 @@ class ToDoListViewController: UITableViewController {
             
         }
     
+    //MARK: - Model manipulation methods
     
     func updateData() {
         do {
@@ -129,19 +129,54 @@ class ToDoListViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    func loadData() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadData(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        
         do {
             itemArray = try context.fetch(request)
         } catch {
             print("Error fetching data from context, \(error)")
         }
+        tableView.reloadData()
 
     }
     
-    func deleteData(row: Int) {
-        context.delete(itemArray[row])
+    
+    
+    
+}
+
+//MARK: - Search Bar Methods
+extension ToDoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        loadSearchResults(search: searchBar.text!)
+   
     }
     
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+           loadData()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+        } else {
+            loadSearchResults(search: searchBar.text!)
+        }
+    }
+    
+    func loadSearchResults(search: String) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", search)
+        
+        //Sort our results
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        
+        loadData(with: request)
+    }
 }
 
